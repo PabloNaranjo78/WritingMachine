74159f97c0b811aa68c62655094f1bc20bffb063
PabloNaranjo78 <pablonaranjo5@estudiantec.cr>
2022-05-01 01:01:43 -0600

    Se añade ventana

--- GUI/MainWindow.py
+++ GUI/MainWindow.py
@@ -1,9 +1,11 @@
 from tkinter import *
+from tkinter import filedialog
 
 
 class MainWindow:
 
     def __init__(self, master):
+        self.file_name = ""
         self.master = master
         self.master.title("Writing machine IDE")
         self.master.geometry("1000x650+0+0")
@@ -14,7 +16,34 @@ class MainWindow:
         self.canvas.place(x=0, y=0)
         self.canvas.configure(bg='#3C3F41')
 
+        self.menu_bar = Menu(self.master,background='blue', fg='white')
+        self.file_menu = Menu(self.menu_bar)
+        self.file_menu.add_command(label="Open...", command=self.openFileMenu)
+        self.file_menu.add_command(label="Save", command=self.saveFile)
+
+        self.menu_bar.add_cascade(label="File", menu=self.file_menu)
+        self.master.config(menu=self.menu_bar)
+
+        self.editor_text_Scrollbar = Scrollbar(self.canvas)
+
+        self.editor_text = Text(self.canvas, width=90, height=20, font=('Lucida Sans Typewriter', 12),
+                                yscrollcommand=self.editor_text_Scrollbar.set)
+
+        self.editor_text.config(bg='#2B2B2B', fg='white')
+        self.editor_text_Scrollbar.config(bg='#2B2B2B')
+
+        self.editor_text_Scrollbar.config(command=self.editor_text.yview)
+        self.editor_text.place(x=40, y=20)
+        self.editor_text_Scrollbar.place(x=950, y=20, height=363)
 
 
 
 
+    def openFileMenu(self):
+        self.file_name = filedialog.askopenfilename(initialdir="/", title="Select file")
+        self.master.title("Writing machine IDE - " + self.file_name)
+        self.editor_text.insert(END, self.file_name)
+        print(self.file_name)
+
+    def saveFile(self):
+        print("guardar")
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from tkinter import *
 import GUI.MainWindow
 
-window = Tk()
-mainWindow = GUI.MainWindow.MainWindow(window)
-window.mainloop()
+
+def main():
+    window = Tk()
+    mainWindow = GUI.MainWindow.MainWindow(window)
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    main()
