2089dcfb84da1b25799791d4437a50ae46ae2760
andyrrr <maxtay27@hotmail.com>
2022-05-30 21:47:55 -0600

    hardware con instrucciones de python

--- ArduinoInstructions/arduino/arduino.ino
+++ ArduinoInstructions/arduino/arduino.ino
@@ -1,11 +1,12 @@
 #include <AccelStepper.h>
 #include <MultiStepper.h>
 #include <Servo.h>
+#include <ArduinoJson.h>
 
 long positions[2]= {0,0}; 
-int color[] = {0,90,190};
+int color[] = {90,0,190};
 int instrucciones[] = {1,2,3,4};
-int instruccionActual = 1;
+int instruccionActual = 0;
 
 AccelStepper motorX(AccelStepper::FULL4WIRE,2,3,5,6);
 AccelStepper motorY(AccelStepper::FULL4WIRE,8,9,10,11);
@@ -17,9 +18,60 @@ MultiStepper steppers;
 float posicionX = 0;
 float posicionY = 0;
 
+class ComandoLapicero{
+    public:
+        bool cambioColor = false;
+        int color = 1;
+
+        int Xmov = 0;
+        int Ymov = 0;
+        String tipo = "";
+        String ultimoColor = "1";
+
+
+        void cargarComando(String comando){
+            String XmovTemp = "";
+            String YmovTemp = "";
+            String colorTemp = "1";
+            int contadorP = 0;
+            tipo = "";
+
+            for (int i = 0; i< comando.length(); i++){
+                if (comando.charAt(i) == '%'){
+                    contadorP++;
+                }
+                else if (contadorP<2){
+                    tipo+=comando.charAt(i);
+                }else if (tipo == "mover"){
+                    if (contadorP<3){
+                        XmovTemp+= comando.charAt(i);
+                    }else if (contadorP<4){
+                        YmovTemp+= comando.charAt(i);
+                    }
+                } else if (tipo == "color"){
+                  if (contadorP<3){
+                        colorTemp = comando.charAt(i);
+                        if (colorTemp == "f"){
+                          colorTemp = ultimoColor;
+                        } else if (colorTemp == "1" || colorTemp == "2"){
+                          ultimoColor = colorTemp;
+                        }
+                    }
+                }
+            }
+
+            Xmov = XmovTemp.toInt();
+            Ymov = YmovTemp.toInt();
+            color = colorTemp.toInt();
+        }
+};
+
+ComandoLapicero comandoLapicero;
+String tempMsg;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  Serial.setTimeout(1);
   motorX.setMaxSpeed(200);
   motorY.setMaxSpeed(200);
   motorX.setAcceleration(200);
@@ -38,40 +90,76 @@ void setup() {
 
 void loop() {
  steppers.moveTo(positions);
+ while (!Serial.available());
  if(!steppers.run()){
-  if (instruccionActual == 1){
+  delay(1);
+  tempMsg = Serial.readString();
+  //Serial.println(tempMsg);
+  comandoLapicero.cargarComando(tempMsg);
+  //Serial.println(comandoLapicero.Ymov);
+  delay(1);
+  Serial.println(comandoLapicero.ultimoColor);
+  if (comandoLapicero.tipo == "mover"){
+    motorX.enableOutputs();
+    motorY.enableOutputs();
+    diagonal(comandoLapicero.Xmov,comandoLapicero.Ymov);
+    steppers.moveTo(positions);
+  } else  if (comandoLapicero.tipo == "color"){
+    colores(comandoLapicero.color);
+  }else{
+    motorX.disableOutputs();
+    motorY.disableOutputs();
     colores(0);
-  lineaX(250);
-  }
-  if (instruccionActual == 3){
+    }
+  /**if (instruccionActual == 1){
+    motorX.enableOutputs();
+    motorY.enableOutputs();
     colores(0);
-  lineaX(-250);
+    lineaX(250);
+    steppers.moveTo(positions);
   }
-  if (instruccionActual == 4){
-    colores(2);
-  lineaY(250);
-  }
-  if (instruccionActual == 2){
-    colores(2);
-  lineaY(-250);
-  }
-  if (instruccionActual == 5){
+  else if (instruccionActual == 3){
+    motorX.enableOutputs();
+    motorY.enableOutputs();
     colores(0);
-  diagonal(230,-250);
+    lineaX(-250);
+    steppers.moveTo(positions);
   }
-  if (instruccionActual == 6){
+  else if (instruccionActual == 4){
+    motorX.enableOutputs();
+    motorY.enableOutputs();
     colores(2);
-  diagonal(-230,250);
+    lineaY(250);
+    steppers.moveTo(positions);
   }
-  if (instruccionActual >= 7){
+  else if (instruccionActual == 2){
+    motorX.enableOutputs();
+    motorY.enableOutputs();
+    colores(2);
+    lineaY(-250);
+    steppers.moveTo(positions);
+  }
+  else if (instruccionActual == 5){
+    motorX.enableOutputs();
+    motorY.enableOutputs();
+    colores(0);
+    diagonal(230,-250);
+    steppers.moveTo(positions);
+  }
+  else if (instruccionActual == 6){
+    motorX.enableOutputs();
+    motorY.enableOutputs();
+    colores(2);
+    diagonal(-230,250);
+    steppers.moveTo(positions);
+  }
+  else {
     motorX.disableOutputs();
     motorY.disableOutputs();
     colores(1);
     }
-  instruccionActual++;
-  delay(1000);
-  }
-  
+  //instruccionActual++;**/
+ }
 }
 
 void lineaX(int pos){
--- ArduinoInstructions/prueba2.py
+++ ArduinoInstructions/prueba2.py
@@ -1,14 +1,48 @@
 # Importing Libraries
 import serial
 import time
-arduino = serial.Serial(port='COM6', baudrate=115200, timeout=.1)
+arduino = serial.Serial(port='COM5', baudrate=115200, timeout=.1)
+traduccion = []
 def write_read(x):
     time.sleep(1)
     arduino.write(bytes(x, 'utf-8'))
     time.sleep(1)
-    data = arduino.readline()
-    return data
-while True:
+    print(arduino.readline())
+
+def send_instruccion(instrucciones):
+    for ins in instrucciones:
+        div = ins.split()
+        print (div)
+        if (div[0] == "ContinueUp"):
+            traduccion.append("%mover%0%-" + div[1])
+        elif (div[0] == "ContinueDown"):
+            traduccion.append("%mover%0%" + div[1])
+        elif (div[0] == "ContinueRight"):
+            traduccion.append("%mover%" + div[1] + "%0")
+        elif (div[0] == "ContinueLeft"):
+            traduccion.append("%mover%-" + (div[1]) + "%0")
+        elif (div[0] == "UseColor"):
+            traduccion.append("%color%" + (div[1]))
+        elif (div[0] == "Up"):
+            traduccion.append("%color%0")
+        elif (div[0] == "Down"):
+            traduccion.append("%color%f")
+        elif (div[0] == "Pos"):
+            traduccion.append("%color%f")
+        else:
+            traduccion.append("%apagar%0")
+
+instrucciones = ["iniciar", "UseColor 1", "ContinueRight 250", "ContinueUp 50", "ContinueLeft 250", "UseColor 2", "ContinueUp 50","ContinueRight 250", "ContinueUp 50", "ContinueLeft 250", "ContinueUp 50","ContinueRight 250", "ContinueDown 250","ContinueLeft 250","apagar", "apagar"]
+instrucciones2 = ["iniciar", "UseColor 2", "ContinueRight 250", "Up", "ContinueLeft 250", "Down", "ContinueRight 250", "Up", "ContinueLeft 250", "apagar", "apagar"]
+
+send_instruccion(instrucciones2)
+print(traduccion)
+
+for i in traduccion:
+    print (write_read(i))
+
+"""while True:
     num = input("Enter a number: ") # Taking input from user
     value = write_read(num)
-    print(value) # printing the value
+    #print(value) # printing the value"""
+
